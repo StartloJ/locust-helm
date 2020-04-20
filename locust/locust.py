3f1e4e16690a1af9479fa1fe489809ef1ed6e90e
Watcharin YangNgam <watcharin@opsta.co.th>
2020-04-20 15:41:51 +0700

    first commit prepare template

---
+++ locust/locust.py
@@ -0,0 +1,20 @@
+from locust import HttpLocust, Locust, TaskSet, task, between
+import json
+
+class MediaService(TaskSet):
+    @task(1)
+    def index(self):
+        with self.client.get("/", catch_response=True) as response:
+            print("HTTP Status" + str(response.status_code))
+            if response.status_code > 400:
+                response.success()
+    @task(2)
+    def healthCheck(self):
+        with self.client.get("/health", catch_response=True) as response:
+            jdata = json.loads(response.content)
+            if response.status_code != 200 and jdata["message"] != "found a file":
+                response.failure("Wrong Response")
+
+class Start(HttpLocust):
+    task_set = MediaService
+    wait_time = between(1,5)
