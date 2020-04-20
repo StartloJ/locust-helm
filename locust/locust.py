8d7bbe0dc9ce466557cf9c455eb69236916686c3
Watcharin YangNgam <watcharin@opsta.co.th>
2020-04-20 18:46:19 +0700

    change class example set endpoint

--- locust/locust.py
+++ locust/locust.py
@@ -1,14 +1,14 @@
 from locust import HttpLocust, Locust, TaskSet, task, between
 import json
 
-class MediaService(TaskSet):
+class HealthCheckAPI(TaskSet):
     @task(1)
     def index(self):
         with self.client.get("/", catch_response=True) as response:
             print("HTTP Status" + str(response.status_code))
             if response.status_code > 400:
                 response.success()
-    @task(2)
+    @task(1)
     def healthCheck(self):
         with self.client.get("/health", catch_response=True) as response:
             jdata = json.loads(response.content)
@@ -16,5 +16,5 @@ class MediaService(TaskSet):
                 response.failure("Wrong Response")
 
 class Start(HttpLocust):
-    task_set = MediaService
+    task_set = HealthCheckAPI
     wait_time = between(1,5)
