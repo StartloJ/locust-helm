24ba9501cf2340da74668620ff13b862b9eefbd3
Watcharin YangNgam <watcharin@opsta.co.th>
2020-05-05 10:58:00 +0700

    add scenario 2

--- locust/locust.py
+++ locust/locust.py
@@ -1,5 +1,6 @@
 from locust import HttpLocust, Locust, TaskSet, task, between, TaskSequence
 import json
+import base64
 
 class OpenHomePage(TaskSet):
   """
@@ -9,6 +10,7 @@ Then get homepage from frontend application with API.
   FAKE_UUID = "a240f6b2-2d32-11ea-978f-2e728ce88125"
   FAKE_TOKEN = "cBRizRaanQg:APA91bG9NnxDekifAnyPPDmVH8XtWC52s1ljUnp8cvqwuJR3Ko2JrB1XDUCNRu9_4GWWO4GsdTdOOtU8HHUwPLjkpB4xWx5bundSG5j4LQwiwmxzaS3O6Fkn9S2IxyCG9e3gK6CV8uqI"
   FAKE_USER_ID = "5dd615b6f343d37ad7da0080"
+
   @task(1)
   def openApp(self):
     headerSet = {"Brand": "","OS": "","Model": "","Install": "","UUID": ""}
@@ -37,7 +39,7 @@ Then get homepage from frontend application with API.
       jdata = json.loads(response.content)
       if response.status_code != 200 and jdata['msg']['description'] != "Success":
         response.failure("Wrong Response, it should message desc is \"Success\".")
-  
+
   @task(2)
   def homepage(self):
     with self.client.get("/homepage", catch_response=True) as response:
@@ -57,14 +59,82 @@ Then get homepage from frontend application with API.
       if response.status_code != 200 and jdata['msg']['description'] != "Success" and not isContentLoad:
         response.failure("Wrong Response, it should message desc is \"Success\" and Content least 1 for all.")
 
-class LoginAccount(TaskSequence):
+class LoginwithAccount(TaskSequence):
   """
   Second scenarios to simulate user action on login account from application.
 This transaction inherit task from [OpenHomePage] first, then send POST request 
 to create user's session to get any action.
   """
+  USER_TOKEN = None
+
+  def __encodeToken(self,strtoken):
+    msgByte = strtoken.encode('utf8')
+    base64Byte = base64.b64encode(msgByte)
+    return base64Byte.decode('utf8')
+  
+  def __decodeToken(self,encodetoken):
+    base64Msg = encodetoken.encode('utf8')
+    msgByte = base64.b64decode(base64Msg)
+    return msgByte.decode('utf8')
+
+  def __getToken(self):
+    return self.__decodeToken(self.USER_TOKEN)
+  
+  def __setToken(self,token):
+    self.USER_TOKEN = self.__encodeToken(token)
+
+  @seq_task(1)
+  def sendCredentials(self):
+    headerSet = {"Content-Type": "application/json"}
+    postBodies = {
+      "username": "0912567652",
+      "password": "Pass1234",
+      "type": "normal",
+      "apple": "rottenApple"
+    }
+    with self.client.post("/login", headers=headerSet, json=postBodies, catch_response=True) as response:
+      jdata = json.loads(response.content)
+      if response.status_code != 200 and jdata['msg']['description'] != "Success":
+        response.failure("Wrong Response, it should message desc is \"Success\".")
+      else:
+        self.__setToken(jdata["data"]["token"])
+  
+  @seq_task(2)
+  def refreshToken(self):
+    headerSet = {"Authorization": "Bearer "+self.__getToken() }
+    with self.client.get("/refresh_token", headers=headerSet, catch_response=True) as response:
+      jdata = json.loads(response.content)
+      isContentLoad = True if 'expire' in jdata['data'] else False
+      if response.status_code != 200 and jdata['msg']['description'] != "Success" and not isContentLoad:
+        response.failure("Wrong Response, it should message desc is \"Success\" and Content least 1 for all.")
+
+  @seq_task(3)
+  def checkPinMobile(self):
+    headerSet = {"Authorization": "Bearer "+self.__getToken() }
+    payload = {
+      "pin": "123451",
+      "pin_confirm": "123451"
+    }
+    with self.client.get("/mobile/check/pin", headers=headerSet, data=payload, catch_response=True) as response:
+      jdata = json.loads(response.content)
+      isContentLoad = True if jdata['data']['has_pin'] == 'true' else False
+      if response.status_code != 200 and jdata['msg']['description'] != "Success" and not isContentLoad:
+        response.failure("Wrong Response, it should message desc is \"Success\" and Content least 1 for all.")
+
+  @seq_task(3)
+  def getProfileMe(self):
+    headerSet = {"Authorization": "Bearer "+self.__getToken() }
+    with self.client.get("/me", headers=headerSet, catch_response=True) as response:
+      jdata = json.loads(response.content)
+      isContentLoad = True if 'id' in jdata['data'] else False
+      if response.status_code != 200 and jdata['msg']['description'] != "Success" and not isContentLoad:
+        response.failure("Wrong Response, it should message desc is \"Success\" and Content least 1 for all.")
+
+
+class UserFirstAction(TaskSequence,OpenHomePage):
+  
   def funcname(self, parameter_list):
-    pass
+    raise NotImplementedError
 class Start(HttpLocust):
-  task_set = OpenHomePage
+  task_set = LoginwithAccount
   wait_time = between(1,5)
